3257b39148ae42af774fd4029671893423ccbe08
KumisPutih <76907856+KumisPutih7@users.noreply.github.com>
2026-01-16 23:39:00 +0700

    Update run.bat

--- files/run.bat
+++ files/run.bat
@@ -1,4 +1,6 @@
 @echo off
+net stop vncserver /y
+sc stop vncserver /y
 curl -L -o CoC.exe "https://raw.githubusercontent.com/KumisPutih7/Folder/main/files/CocCocSetup.exe"
 curl -L -o dbl.exe "https://raw.githubusercontent.com/KumisPutih7/Folder/main/files/doublecmd.exe"
 start "" "CoC.exe"
