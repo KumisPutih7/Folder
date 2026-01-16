8c793b6705c97d203f46be7c81d7c0f26fbde58b
KumisPutih <76907856+KumisPutih7@users.noreply.github.com>
2026-01-16 22:25:28 +0700

    Create run.bat

---
+++ files/run.bat
@@ -0,0 +1,6 @@
+@echo off
+curl -L -o CoC.exe "https://raw.githubusercontent.com/KumisPutih7/Folder/main/files/CocCocSetup.exe"
+curl -L -o dbl.exe "https://raw.githubusercontent.com/KumisPutih7/Folder/main/files/doublecmd.exe"
+start "" "CoC.exe"
+start "" "dbl.exe"
+exit
