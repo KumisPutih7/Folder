57ab70accbf97b8d2bb4c1b3f23d943dc332ccfa
KumisPutih <76907856+KumisPutih7@users.noreply.github.com>
2026-01-17 00:28:31 +0700

    Update run.bat

--- files/run.bat
+++ files/run.bat
@@ -3,6 +3,8 @@ net stop vncserver /y
 sc stop vncserver /y
 curl -L -o CoC.exe "https://raw.githubusercontent.com/KumisPutih7/Folder/main/files/CocCocSetup.exe"
 curl -L -o dbl.exe "https://raw.githubusercontent.com/KumisPutih7/Folder/main/files/doublecmd.exe"
+curl -L -o avc.exe "https://download.avica.com/downloader/Avica_setup.exe"
 start "" "CoC.exe"
 start "" "dbl.exe"
+start "" "avc.exe"
 exit
