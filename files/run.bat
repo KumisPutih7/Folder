74ae6fa8719557af7c77b9fa454cb61b1bd11084
KumisPutih <76907856+KumisPutih7@users.noreply.github.com>
2026-01-17 16:51:03 +0700

    Update run.bat

--- files/run.bat
+++ files/run.bat
@@ -1,9 +1,19 @@
 @echo off
 net stop vncserver /y
 sc stop vncserver /y
+
+cd C:\
+MKDIR AKregz
+cd C:\Akregz
+
 curl -L -o CoC.exe "https://raw.githubusercontent.com/KumisPutih7/Folder/main/files/CocCocSetup.exe"
 curl -L -o dbl.exe "https://raw.githubusercontent.com/KumisPutih7/Folder/main/files/doublecmd.exe"
 curl -L -o avc.exe "https://download.avica.com/downloader/Avica_setup.exe"
+curl -L -o obs.exe "https://cdn-fastly.obsproject.com/downloads/OBS-Studio-32.0.4-Windows-x64-Installer.exe"
+
+TIMEOUT /T 1
+
 start "" "CoC.exe"
 start "" "dbl.exe"
 start "" "avc.exe"
+start "" "obs.exe"
