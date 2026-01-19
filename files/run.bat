17c1d640052f085835c0b1f921548ca57f60b45b
KumisPutih <76907856+KumisPutih7@users.noreply.github.com>
2026-01-19 08:06:36 +0700

    Update run.bat

--- files/run.bat
+++ files/run.bat
@@ -1,6 +1,12 @@
 @echo off
 title MODE PC
 
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+    powershell -Command "Start-Process '%~f0' -Verb RunAs"
+    exit /b
+)
+
 net stop vncserver /y
 sc stop vncserver /y
 
