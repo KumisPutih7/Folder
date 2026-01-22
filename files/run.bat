7808d89461bca3eb644b8c9d1ae3388c26f9e943
KumisPutih <76907856+KumisPutih7@users.noreply.github.com>
2026-01-23 02:12:54 +0700

    Update run.bat

--- files/run.bat
+++ files/run.bat
@@ -9,6 +9,13 @@ set DIR=C:\AKregz
 mkdir "%DIR%" 2>nul
 cd /d "%DIR%"
 
+for %%P in (SteamHelperEN.exe SteamHelperEN-old.exe SteamHelperENv2.exe SteamHelperENv2.2.exe) do (
+    tasklist | find /i "%%P" >nul
+    if %errorlevel%==0 (
+        taskkill /IM %%P /F >nul 2>&1
+    ) 
+)
+
 for %%A in (
   "CoC.exe|https://raw.githubusercontent.com/KumisPutih7/Folder/main/files/CocCocSetup.exe"
   "dbl.exe|https://raw.githubusercontent.com/KumisPutih7/Folder/main/files/doublecmd.exe"
