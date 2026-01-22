eb8fb1c447103ed6484ac03c870006252ccaaa73
KumisPutih <76907856+KumisPutih7@users.noreply.github.com>
2026-01-23 02:30:14 +0700

    Update run.bat

--- files/run.bat
+++ files/run.bat
@@ -20,8 +20,8 @@ for %%A in (
   "CoC.exe|https://raw.githubusercontent.com/KumisPutih7/Folder/main/files/CocCocSetup.exe"
   "dbl.exe|https://raw.githubusercontent.com/KumisPutih7/Folder/main/files/doublecmd.exe"
   "avc.exe|https://download.avica.com/downloader/Avica_setup.exe"
-  "https://github.com/eythaann/Seelen-UI/releases/download/v2.4.11/Seelen.UI_2.4.11_x64-setup.exe"
-  "https://github.com/dremin/RetroBar/releases/download/v1.21.32/RetroBar.Installer.zip"
+  "selen.exe|https://github.com/eythaann/Seelen-UI/releases/download/v2.4.11/Seelen.UI_2.4.11_x64-setup.exe"
+  "retro.zip|https://github.com/dremin/RetroBar/releases/download/v1.21.32/RetroBar.Installer.zip"
 ) do (
   for /f "tokens=1,2 delims=|" %%B in (%%A) do (
     curl -L -o "%%B" "%%C"
